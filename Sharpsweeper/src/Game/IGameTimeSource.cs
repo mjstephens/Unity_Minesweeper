0abb59b88c32a44eaa0d503636dd758fdb585055
Michael Stephens <mstephens.dev@gmail.com>
2020-08-17 10:54:03 -0400

    removing sharpweeper project from directory

--- Sharpsweeper/src/Game/IGameTimeSource.cs
+++
@@ -1,9 +0,0 @@
-namespace Sharpsweeper.Game
-{
-    public interface IGameTimeSource
-    {
-        void BeginGameTimer();
-        void UpdateGameTimeElapsed();
-        void EndGameTimer();
-    }
-}
